357664e021b2c86cb103bcfc82305328359651b2
Ville Takanen <vitkukissa@gmail.com>
2025-01-04 18:53:08 +0200

    chore: project setup done

--- src/cn-story-clock.ts
+++ src/cn-story-clock.ts
@@ -1,1 +1,11 @@
-export class CnStoryClock {}
+import { LitElement, html } from "lit";
+import { customElement } from "lit/decorators.js";
+
+@customElement("cn-story-clock")
+export class CnStoryClock extends LitElement {
+  render() {
+    return html`
+      <p>This is a rudimentary story clock.</p>
+    `;
+  }
+}
