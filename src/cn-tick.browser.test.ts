b2a84464010e4d348219d56812c1cf57d530ba40
Ville Takanen <vitkukissa@gmail.com>
2025-08-18 10:59:53 +0300

    feat: updated to cyan v4 compatibility

---
+++ src/cn-tick.browser.test.ts
@@ -0,0 +1,15 @@
+import { describe, expect, it } from 'vitest';
+import { html } from 'lit';
+
+import './cn-tick';
+
+describe('cn-tick', () => {
+  it('should be visually hidden', async () => {
+    document.body.innerHTML = '<cn-tick></cn-tick>';
+    const el = document.body.querySelector('cn-tick');
+    await customElements.whenDefined('cn-tick');
+    expect(el).not.toBeNull();
+    const style = getComputedStyle(el!)
+    expect(style.display).toBe('none');
+  });
+});
